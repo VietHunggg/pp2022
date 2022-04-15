40ea0d5648d78e6a4673a0a0c905c883d642cffa
Nguyen Viet Hung <vhungpmpx@gmail.com>
2022-04-15 17:10:35 +0700

    Till fixxing

--- 7.shell.py
+++ 7.shell.py
@@ -1,14 +1,27 @@
 import os
 import subprocess
+import re
+from sys import stdout 
+
+def proc_input(inp, arr):
+    tpm = inp
+    x = tpm.split(' ')
+    for i in x:
+        arr.append(i)
+    return arr
+
 
 def main():
-    print("Commandline input: ")
     while True:
+        tpmArr = []
         cmd = input(">")
-        cmdread = subprocess.run(f"{cmd}", shell=True)
+        proc_input(cmd, tpmArr)
+        cmdread = subprocess.run(proc_input(cmd, tpmArr), shell=True, stdout= subprocess.PIPE, stderr=subprocess.PIPE)
         print(f"Return {cmdread.returncode}")
         print(f"Stdout: {cmdread.stdout}")
         print(f"Check return code: {cmdread.returncode}")
+        print(cmdread.stdout.decode("utf-8"))
+        
 
 if __name__ == "__main__":
     main()
